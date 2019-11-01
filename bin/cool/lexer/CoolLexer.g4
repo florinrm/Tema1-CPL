5b86f498aa7f6f154070a80ac9c237762a9ae04b
Florin Mihalache <florinrmfm3@gmail.com>
2019-11-01 23:51:43 +0200

    Added files

---
+++ bin/cool/lexer/CoolLexer.g4
@@ -0,0 +1,145 @@
+lexer grammar CoolLexer;
+
+tokens { ERROR } 
+
+@header{
+    package cool.lexer;	
+}
+
+@members{    
+    private void raiseError(String msg) {
+        setText(msg);
+        setType(ERROR);
+    }
+}
+
+/* Cuvânt cheie.
+ */
+IF : 'if';
+THEN : 'then';
+ELSE : 'else';
+FI: 'fi';
+NOT: 'not';ls
+
+NEW : 'new';
+CLASS : 'class';
+INHERITS: 'inherits';
+
+LET : 'let';
+IN : 'in';
+
+SELFTYPE: 'SELF_TYPE';
+
+WHILE: 'while';
+LOOP: 'loop';
+POOL: 'pool';
+
+CASE: 'case';
+OF: 'of';
+ESAC: 'esac';
+
+BOOL : 'true' | 'false';
+
+TYPE : 'Int' | 'Float' | 'Bool';
+
+/* Identificator.
+ */
+fragment LETTER : [a-zA-Z];
+ID : (LETTER | '_')(LETTER | '_' | DIGIT)*;
+
+/* Număr întreg.
+ *
+ * fragment spune că acea categorie este utilizată doar în interiorul
+ * analizorului lexical, nefiind trimisă mai departe analizorului sintactic.
+ */
+fragment DIGIT : [0-9];
+INT : DIGIT+;
+
+/* Număr real.
+ */
+fragment DIGITS : DIGIT+;
+fragment EXPONENT : 'e' ('+' | '-')? DIGITS;
+FLOAT : (DIGITS ('.' DIGITS?)? | '.' DIGITS) EXPONENT?;
+
+/* Șir de caractere.
+ *
+ * Poate conține caracterul '"', doar precedat de backslash.
+ * . reprezintă orice caracter în afară de EOF.
+ * *? este operatorul non-greedy, care încarcă să consume caractere cât timp
+ * nu a fost întâlnit caracterul ulterior, '"'.
+ *
+ * Acoladele de la final pot conține secvențe arbitrare de cod Java,
+ * care vor fi executate la întâlnirea acestui token.
+ */
+STRING : '"' ('\\"' | .)*? '"'
+    { System.out.println("there are no strings in CPLang, but shhh.."); };
+
+SEMI : ';';
+
+COMMA : ',';
+
+ASSIGN : '<-';
+
+LPAREN : '(';
+
+RPAREN : ')';
+
+LBRACE : '{';
+
+RBRACE : '}';
+
+PLUS : '+';
+
+MINUS : '-';
+
+MULT : '*';
+
+DIV : '/';
+
+EQUAL : '=';
+
+LT : '<';
+
+LE : '<=';
+
+fragment NEW_LINE : '\r'? '\n';
+
+LINE_COMMENT: '//' .*? (NEW_LINE | EOF) -> skip;
+
+BLOCK_COMMENT
+    : '/*'
+      (BLOCK_COMMENT | .)*?
+      ('*/' | EOF { System.err.println("EOF in comment"); }) -> skip
+    ;
+
+/* Spații albe.
+ *
+ * skip spune că nu este creat niciun token pentru lexemul depistat.
+ */
+WS : [ \n\r\t]+ -> skip;
+
+/* Modalitate alternativă de recunoaștere a șirurilor de caractere, folosind
+ * moduri lexicale.
+ *
+ * Un mod lexical, precum cel implicit (DEFAULT_MODE) sau IN_STR, de mai jos,
+ * reprezintă stări ale analizorului. Când analizorul se află într-un anumit
+ * mod, numai regulile din acel mod se pot activa.
+ *
+ * Folosim pushMode și popMode pentru intra și ieși din modurile lexicale,
+ * în regim de stivă.
+ *
+ * more spune că deocamdată nu este construit un token, dar lexemul identificat
+ * va face parte, cumulativ, din lexemul recunoscut de următoarea regulă.
+ *
+ * De-abia la recunoașterea caracterului '"' de sfârșit de șir de către regula
+ * STR, se va construi un token cu categoria STR și întregul conținut al șirului
+ * drept lexem.
+ */
+/*
+STR_OPEN : '"' -> pushMode(IN_STR), more;
+
+mode IN_STR;
+
+STR : '"' -> popMode;
+CHAR : ('\\"' | ~'"') -> more;  // ~ = complement
+*/
---
+++ src/cool/parser/CoolParser.java
@@ -0,0 +1,168 @@
+// Generated from CoolParser.g4 by ANTLR 4.7.2
+
+package cool.parser;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.ATN;
+import org.antlr.v4.runtime.atn.ATNDeserializer;
+import org.antlr.v4.runtime.atn.ParserATNSimulator;
+import org.antlr.v4.runtime.atn.PredictionContextCache;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class CoolParser extends Parser {
+    public static final int
+            ERROR = 1, WS = 2;
+    public static final int
+            RULE_program = 0;
+    public static final String[] ruleNames = makeRuleNames();
+    /**
+     * @deprecated Use {@link #VOCABULARY} instead.
+     */
+    @Deprecated
+    public static final String[] tokenNames;
+    public static final String _serializedATN =
+            "\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\4\7\4\2\t\2\3\2\3" +
+                    "\2\3\2\2\2\3\2\2\2\2\5\2\4\3\2\2\2\4\5\7\2\2\3\5\3\3\2\2\2\2";
+    public static final ATN _ATN =
+            new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+    protected static final DFA[] _decisionToDFA;
+    protected static final PredictionContextCache _sharedContextCache =
+            new PredictionContextCache();
+    private static final String[] _LITERAL_NAMES = makeLiteralNames();
+    private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
+    public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+    static {
+        RuntimeMetaData.checkVersion("4.7.2", RuntimeMetaData.VERSION);
+    }
+
+    static {
+        tokenNames = new String[_SYMBOLIC_NAMES.length];
+        for (int i = 0; i < tokenNames.length; i++) {
+            tokenNames[i] = VOCABULARY.getLiteralName(i);
+            if (tokenNames[i] == null) {
+                tokenNames[i] = VOCABULARY.getSymbolicName(i);
+            }
+
+            if (tokenNames[i] == null) {
+                tokenNames[i] = "<INVALID>";
+            }
+        }
+    }
+
+    static {
+        _decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+        for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+            _decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+        }
+    }
+
+    public CoolParser(TokenStream input) {
+        super(input);
+        _interp = new ParserATNSimulator(this, _ATN, _decisionToDFA, _sharedContextCache);
+    }
+
+    private static String[] makeRuleNames() {
+        return new String[]{
+                "program"
+        };
+    }
+
+    private static String[] makeLiteralNames() {
+        return new String[]{
+        };
+    }
+
+    private static String[] makeSymbolicNames() {
+        return new String[]{
+                null, "ERROR", "WS"
+        };
+    }
+
+    @Override
+    @Deprecated
+    public String[] getTokenNames() {
+        return tokenNames;
+    }
+
+    @Override
+
+    public Vocabulary getVocabulary() {
+        return VOCABULARY;
+    }
+
+    @Override
+    public String getGrammarFileName() {
+        return "CoolParser.g4";
+    }
+
+    @Override
+    public String[] getRuleNames() {
+        return ruleNames;
+    }
+
+    @Override
+    public String getSerializedATN() {
+        return _serializedATN;
+    }
+
+    @Override
+    public ATN getATN() {
+        return _ATN;
+    }
+
+    public final ProgramContext program() throws RecognitionException {
+        ProgramContext _localctx = new ProgramContext(_ctx, getState());
+        enterRule(_localctx, 0, RULE_program);
+        try {
+            enterOuterAlt(_localctx, 1);
+            {
+                setState(2);
+                match(EOF);
+            }
+        } catch (RecognitionException re) {
+            _localctx.exception = re;
+            _errHandler.reportError(this, re);
+            _errHandler.recover(this, re);
+        } finally {
+            exitRule();
+        }
+        return _localctx;
+    }
+
+    public static class ProgramContext extends ParserRuleContext {
+        public ProgramContext(ParserRuleContext parent, int invokingState) {
+            super(parent, invokingState);
+        }
+
+        public TerminalNode EOF() {
+            return getToken(CoolParser.EOF, 0);
+        }
+
+        @Override
+        public int getRuleIndex() {
+            return RULE_program;
+        }
+
+        @Override
+        public void enterRule(ParseTreeListener listener) {
+            if (listener instanceof CoolParserListener) ((CoolParserListener) listener).enterProgram(this);
+        }
+
+        @Override
+        public void exitRule(ParseTreeListener listener) {
+            if (listener instanceof CoolParserListener) ((CoolParserListener) listener).exitProgram(this);
+        }
+
+        @Override
+        public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+            if (visitor instanceof CoolParserVisitor)
+                return ((CoolParserVisitor<? extends T>) visitor).visitProgram(this);
+            else return visitor.visitChildren(this);
+        }
+    }
+}
